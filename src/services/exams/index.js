d97fb74e12026252e3b862ca8edc13be2e5c3ff4
Abdug'affor Abdurahimov <abdurahimov.97@list.ru>
2021-01-07 14:29:58 +0500

    shuffle func created

--- src/services/exams/index.js
+++ src/services/exams/index.js
@@ -1,12 +1,14 @@
 const express = require("express");
 const { getQuestions } = require("../../fsUtilities");
+const shuffleArray = require("../../helperFunctons");
 
 const examsRouter = express.Router();
 
 examsRouter.post("/start", async (req, res, next) => {
   try {
     const qeuestions = await getQuestions();
-    console.log(qeuestions);
+    randQuestions = shuffleArray(qeuestions, 5);
+    console.log(randQuestions);
     res.send("start");
   } catch (error) {
     next(error);
